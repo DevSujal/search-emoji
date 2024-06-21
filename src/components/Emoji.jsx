719b29f58e270dfe67551474b23f08c20f7f1f04
DevSujal <nimjesujal2004@gmail.com>
2024-06-21 09:58:36 +0530

    "Refactor Emoji component: reformatted code, added new conditions to emoji filter, and updated hover effect implementation."

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -17,43 +17,62 @@ function Emoji({ category }) {
           emojis.push({ emoji: data[i].htmlCode[0], name: data[i].name });
         }
         dispatch(addAllCategoryEmoji({ emojis, category }));
-      }).finally(() => setLoader(false))
+      })
+      .finally(() => setLoader(false));
   }, [emojis]);
   const dispatch = useDispatch();
 
-
   const hover = (e) => {
-    const target = e.target.parentElement.firstElementChild
-    target.classList.remove("hidden")
-    const {top} = e.target.getBoundingClientRect()
-    const scrollY = window.scrollY
-    target.style.top = `${scrollY + top - 40}px`
-   } // it is the code for hovering effect on perticular emojis
+    const target = e.target.parentElement.firstElementChild;
+    target.classList.remove("hidden");
+    const { top } = e.target.getBoundingClientRect();
+    const scrollY = window.scrollY;
+    target.style.top = `${scrollY + top - 40}px`;
+  }; // it is the code for hovering effect on perticular emojis
   const notHover = (e) => {
-    const target = e.target.parentElement.firstElementChild
-    target.classList.add("hidden")
-   }
-  
+    const target = e.target.parentElement.firstElementChild;
+    target.classList.add("hidden");
+  };
+
   if (loader) {
-    return <div className="absolute transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"><h1 className=" text-5xl text-white text-bold">Loading...</h1></div>;
+    return (
+      <div className="absolute transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className=" text-5xl text-white text-bold">Loading...</h1>
+      </div>
+    );
   } else {
     return (
       <div className="flex flex-wrap w-11/12 m-auto justify-around gap-7 my-7">
-        {emojis.filter((emoji) => (
-            emoji.name.toLowerCase().includes(search.toLowerCase()) && emoji.name !== "white smiling face ≊ smiling face"
-        )).map((emoji) => (
-          <div
-            key={emoji.id}
-            className=" max-w-40 shadow-md flex flex-col justify-center bg-gray-950 rounded-xl items-center  p-3 pb-5 hover:cursor-pointer"
-          >
-            <h2 className="absolute  text-gray-200 font-semibold hidden z-10">{emoji.name}</h2>
-            <h2 className="text-7xl hover:cursor-pointer sm:text-8xl" onMouseOver = {hover} onMouseLeave={notHover}>
-              {String.fromCodePoint(
-                parseInt(emoji.emoji.replace(/^&#(\d+);$/g, "$1"))
-              )}
-            </h2>
-          </div>
-        ))}
+        {emojis
+          .filter(
+            (emoji) =>
+              emoji.name.toLowerCase().includes(search.toLowerCase()) &&
+              emoji.name !== "white smiling face ≊ smiling face" &&
+              emoji.name !== "white froning face ≊ frowning face" &&
+              emoji.name !== "skull and crossbones" &&
+              emoji.name !==
+                "heavy heart exclamation mark ornament ≊ heavy heart exclamation" &&
+              emoji.name !== "helmet with white cross"
+          )
+          .map((emoji) => (
+            <div
+              key={emoji.id}
+              className=" max-w-40 shadow-md flex flex-col justify-center bg-gray-950 rounded-xl items-center  p-3 pb-5 hover:cursor-pointer"
+            >
+              <h2 className="absolute  text-gray-200 font-semibold hidden z-10">
+                {emoji.name}
+              </h2>
+              <h2
+                className="text-7xl hover:cursor-pointer sm:text-8xl"
+                onMouseOver={hover}
+                onMouseLeave={notHover}
+              >
+                {String.fromCodePoint(
+                  parseInt(emoji.emoji.replace(/^&#(\d+);$/g, "$1"))
+                )}
+              </h2>
+            </div>
+          ))}
       </div>
     );
   }
